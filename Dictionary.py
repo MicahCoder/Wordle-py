796cc67592bb0e5eccd3d570fb75e5b940cd286e
MicahCoder <micahgruenwald@gmail.com>
2025-04-29 19:50:06 -0600

    Dict Class

---
+++ Dictionary.py
@@ -0,0 +1,14 @@
+import random
+
+class Dictionary:
+    def __init__(self):
+        self.legalWords = None
+        self.wordleWords = None
+    def checkWord(self, word):
+        if(self.legalWords == None):
+             self.legalWords = open("assets/5LetterDict.txt","r").read().splitlines()
+        return word in self.wordleWords
+    def getRandomWord(self):
+        if(self.wordleWords == None):
+            self.wordleWords = open("assets/wordleWords.txt","r").read().splitlines()
+        return random.choice(self.wordleWords)
